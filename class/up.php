76b468fe555f857936031edf8b32f33d14db08a6
lttjames <33799375+lttjames@users.noreply.github.com>
2018-10-14 21:26:35 +0800

    nmc

---
+++ class/up.php
@@ -0,0 +1,13 @@
+<?php 
+$_POST['check23'];
+
+echo json_encode($_POST);
+
+/*$con=mysqli_connect("localhost","root","","test");
+if (!$con) 
+{ 
+    die("连接错误: " . mysqli_connect_error()); 
+} 
+$sql="create table test";
+$con->query($sql);
+*/
